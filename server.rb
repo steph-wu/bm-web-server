7fed0846bd9bf57210be1601f572a47dd7165223
STEPHANIE <WU.STEPHANIE@LIVE.COM>
2017-01-24 15:02:33 -0500

    Create index html page

--- server.rb
+++ server.rb
@@ -15,6 +15,8 @@ loop do                                             # Server runs forever
   end
   puts lines                                        # Output the full request to stdout
 
-  client.puts(Time.now.ctime)                       # Output the current time to the client
+  filename = "index.html"
+  response = File.read(filename)
+  client.puts(response)                     # Output the current time to the client
   client.close                                      # Disconnect from the client
 end
